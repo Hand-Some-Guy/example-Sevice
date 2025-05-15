4cfd4056dcf874780e784c6e0fde4136a774fc90
Hand-Some-Guy <asd561298@gmail.com>
2025-05-15 05:27:57 +0000

    주석 수정

--- models/type.go
+++ models/type.go
@@ -3,14 +3,14 @@ package models
 type Sevice string
 
 const (
-    NEW Sevice = "NEW"  # 신규 디바이스 
-    OLD Sevice = "OLD"  # 구형 디바이스 
+    NEW Sevice = "NEW" // 신규 디바이스 
+    OLD Sevice = "OLD" // 구형 디바이스 
 )
 
 type DeviceStatus string
 
 const (
-    Pending   DeviceStatus = "PENDING"    # 대기 상태 
-    Completed DeviceStatus = "COMPLETED"  # 업데이트 완료 상태 
-    Failed    DeviceStatus = "FAILED"     # 업데이트 실패 상태 
+    Pending   DeviceStatus = "PENDING"    //  대기 상태 
+    Completed DeviceStatus = "COMPLETED"  //  업데이트 완료 상태 
+    Failed    DeviceStatus = "FAILED"     //  업데이트 실패 상태 
 )
