0587ae8c7bfff762fc1cad32d61ec6bd0b380026
Hand-Some-Guy <asd561298@gmail.com>
2025-05-15 04:05:32 +0000

    경로 검사 로직 수정

--- models/firmware.go
+++ models/firmware.go
@@ -39,43 +39,41 @@ func NewFirmware(id string, serviceType Sevice, version, path string) (*Firmware
 // 경로 유효성 검사
 func (f *Firmware) ValidateFilePath(path string) error {
 
-	// Basic path checks
-    if path == "" {
-        return fmt.Errorf("경로를 반드시 입력해 주세요. ")
-    }
-    if strings.Contains(path, "\x00") {
-        return fmt.Errorf("null을 포함할 수 없습니다.")
-    }
-    if !strings.HasPrefix(path, "/") {
-        return fmt.Errorf("linux 포멧에 맞는 경로가 필요합니다 .")
-    }
+    // if path == "" {
+    //     return fmt.Errorf("경로를 반드시 입력해 주세요. ")
+    // }
+    // if strings.Contains(path, "\x00") {
+    //     return fmt.Errorf("null을 포함할 수 없습니다.")
+    // }
+    // if !strings.HasPrefix(path, "/") {
+    //     return fmt.Errorf("linux 포멧에 맞는 경로가 필요합니다 .")
+    // }
 
-    // Check for invalid characters
-    invalidChars := regexp.MustCompile(`[<>\|\*\?\\:;]`)
-    if invalidChars.MatchString(path) {
-        return fmt.Errorf("path must have .bin or .fw extension")
-    }
+    // invalidChars := regexp.MustCompile(`[<>\|\*\?\\:;]`)
+    // if invalidChars.MatchString(path) {
+    //     return fmt.Errorf("path must have .bin or .fw extension")
+    // }
 
-	// 파일 포맷 검사 
-	if !strings.HasSuffix(path, ".bin") && !strings.HasSuffix(path, ".fw") {
-        return fmt.Errorf("path must have .bin or .fw extension")
-    }
+	// // 파일 포맷 검사 
+	// if !strings.HasSuffix(path, ".bin") && !strings.HasSuffix(path, ".fw") {
+    //     return fmt.Errorf("path must have .bin or .fw extension")
+    // }
 
-	// 경로가 실재로 존재하는지 검증 
-	// 테스트 환경에서 제외 
-	// info, err := os.Stat(path)
-	// if err != nil {
-	// 	return  fmt.Errorf("failed to path: %w", err)
+	// // 경로가 실재로 존재하는지 검증 
+	// // 테스트 환경에서 제외 
+	// // info, err := os.Stat(path)
+	// // if err != nil {
+	// // 	return  fmt.Errorf("failed to path: %w", err)
+	// // }
+	// // if info.IsDir() {
+	// // 	return fmt.Errorf("path '%s' is a directory, not a file", path)
+	// // }
+
+	// // 경로 정규화
+	// cleanPath := filepath.Clean(path)
+	// if cleanPath == "" || cleanPath == "/" {
+	// 	return fmt.Errorf("해당 경로는 정규화 할 수 없습니다")
 	// }
-	// if info.IsDir() {
-	// 	return fmt.Errorf("path '%s' is a directory, not a file", path)
-	// }
-
-	// 경로 정규화
-	cleanPath := filepath.Clean(path)
-	if cleanPath == "" || cleanPath == "/" {
-		return fmt.Errorf("해당 경로는 정규화 할 수 없습니다")
-	}
 
 	return nil
 
